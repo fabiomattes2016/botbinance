9f4503c92a2330d9e8eaeaefa98d379dd030b7fb
Fabio Mattes <fabio.mattes@benner.com.br>
2022-10-06 20:49:17 -0300

    first commit

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ async function loadData(path) {
 
 setInterval(function () {
   try {
-    http.get("http://herokuappname.herokuapp.com/api/");
+    http.get("https://bot-binance-fabio.herokuapp.com/api");
   } catch (err) {
     console.error(err);
   }
